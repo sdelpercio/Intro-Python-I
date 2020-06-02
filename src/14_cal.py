c89e61130655c0262a4affa326927eb70dce1d10
Shawn DelPercio <sdelperc@gmail.com>
2020-06-02 18:41:06 -0400

    finished files 11 through 15

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,40 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+# get month and year from user
+current_month = datetime.now().month
+current_year = datetime.now().year
+c = calendar.TextCalendar(calendar.SUNDAY)
+
+# Each input given by the user gets appended to the list
+x = input().split()
+
+# if no inputs, print out a calendar for current date
+if not x:
+    print(c.formatmonth(current_year, current_month))
+elif len(x) == 1:
+    print(c.formatmonth(current_year, int(x[0])))
+elif len(x) == 2:
+    print(c.formatmonth(int(x[1]), int(x[0])))
+else:
+    print('Please enter a month followed by a year')
+
+
+# if one input, assume month, print calendar for that month
+
+# if two inputs, assume month year, print calendar for that month/year
+
+# if more than two inputs, print usage
